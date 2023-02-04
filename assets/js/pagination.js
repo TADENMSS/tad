5fae743bccd65a79e6479f00b94974c4b62403c7
Rafael Abonce <70127910+mrraxyer@users.noreply.github.com>
2023-02-03 22:20:16 -0600

    version 0.3

---
+++ assets/js/pagination.js
@@ -0,0 +1,33 @@
+// Visualize: Pagination (Next/Previous)
+
+visualize({
+    auth: {
+        name: "joeuser",
+        password: "joeuser",
+        organization: "organization_1"
+    }
+}, function (v) {
+    
+    var report = v.report({
+        resource: "/public/Samples/Reports/AllAccounts",
+        container: "#container"
+    });
+    
+    $("#previousPage").click(function() {
+        var currentPage = report.pages() || 1;
+        
+        report
+            .pages(--currentPage)
+            .run()
+                .fail(function(err) { alert(err); });
+    });
+    
+    $("#nextPage").click(function() {
+        var currentPage = report.pages() || 1;
+        
+        report
+            .pages(++currentPage)
+            .run()
+                .fail(function(err) { alert(err); });
+    });
+});
